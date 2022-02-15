3c517b4657eb51a13b1bce5a8d782fa26e5a4572
gldeA <glde.abbott@gmail.com>
2022-02-14 18:00:03 -0600

    Enacted all changes requested by Quinten before merge into main

--- robot.py
+++ robot.py
@@ -1,29 +1,59 @@
-import ctre
+
 import wpilib
+import ctre
+from wpilib import interfaces
 
 
 class MyRobot(wpilib.TimedRobot):
-
     def testInit(self):
-        self.motor0 = ctre.WPI_TalonFX(0)
-        self.motor1 = ctre.WPI_TalonFX(1)
-        self.motor2 = ctre.WPI_TalonFX(2)
-        self.motor3 = ctre.WPI_TalonFX(3)
-        self.joystick = wpilib.Joystick(0)
-        self.joystick2 = wpilib.Joystick(1)
+        self.back_right = ctre.WPI_TalonFX(0)
+        self.front_left = ctre.WPI_TalonFX(1)
+        self.back_left = ctre.WPI_TalonFX(2)
+        self.front_right = ctre.WPI_TalonFX(3)
+        self.intake = ctre.WPI_TalonFX(4)
+        self.shooter_angle_1 = ctre.WPI_TalonSRX(11)
+        self.shooter_angle_2 = ctre.WPI_TalonSRX(10)
+        self.front_right.setInverted(True)
+        self.back_right.setInverted(True)
 
-        self.motor0.setInverted(True)
-        self.motor3.setInverted(True)
-    
+        #controller groups
+        self.shooter_angle = wpilib.MotorControllerGroup(self.shooter_angle_1, self.shooter_angle_2)
+
+        #controller variables
+        self.controller = wpilib.XboxController(0)
+        self.controllerHID = interfaces.GenericHID(0)
+
+        self.drive_speed = .5
+
+
     def testPeriodic(self):
-        self.multiplier = .25
-        #*self.multiplier
-        self.motor0.set((self.joystick.getY() - self.joystick.getX() - self.joystick2.getX()) * self.multiplier)
-        self.motor1.set((self.joystick.getY() + self.joystick.getX() + self.joystick2.getX()) * self.multiplier)
-        self.motor2.set((self.joystick.getY() - self.joystick.getX() + self.joystick2.getX()) * self.multiplier)
-        self.motor3.set((self.joystick.getY() + self.joystick.getX() - self.joystick2.getX()) * self.multiplier)
+        self.front_right.set((self.controller.getLeftY() - self.controller.getLeftX() - self.controller.getRightX()) * self.drive_speed)
+        self.front_left.set((self.controller.getLeftY() + self.controller.getLeftX() + self.controller.getRightX()) * self.drive_speed)
+        self.back_left.set((self.controller.getLeftY() - self.controller.getLeftX() + self.controller.getRightX()) * self.drive_speed)
+        self.back_right.set((self.controller.getLeftY() + self.controller.getLeftX() - self.controller.getRightX()) * self.drive_speed)
         
+        if self.controllerHID.getPOV() == -1 or self.controllerHID.getPOV() == 90 or self.controllerHID.getPOV() == 270:
+            self.shooter_angle.set(0)
+        elif self.controllerHID.getPOV() == 0:
+            self.shooter_angle.set(-.5)
+        elif self.controllerHID.getPOV() == 180:
+            self.shooter_angle.set(.5)
+
+        if self.controller.getRightTriggerAxis() > self.controller.getLeftTriggerAxis():
+            self.intake.set(self.controller.getRightTriggerAxis() )
+
+        if self.controller.getRightTriggerAxis() < self.controller.getLeftTriggerAxis():
+            self.intake.set(self.controller.getLeftTriggerAxis() * -1 )
+
+        if self.controller.getRightTriggerAxis() == self.controller.getLeftTriggerAxis():
+            self.intake.set(0)
+
+        if self.controller.getRightTriggerAxis() != 0:
+            self.controllerHID.setRumble(interfaces.GenericHID.RumbleType.kRightRumble, 1)
+            self.controllerHID.setRumble(interfaces.GenericHID.RumbleType.kLeftRumble, 1)
+        else:
+            self.controllerHID.setRumble(interfaces.GenericHID.RumbleType.kRightRumble, 0)
+            self.controllerHID.setRumble(interfaces.GenericHID.RumbleType.kLeftRumble, 0)
 
 if __name__ == "__main__":
     wpilib.run(MyRobot)
-    
